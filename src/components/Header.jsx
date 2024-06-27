48bfa2b66bb7e828d96798676aa5f4bf1d853613
rd273001 <rd273001@gmail.com>
2024-06-28 02:58:28 +0530

    Wrap logo image with anchor tag in Header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import logo from '../assets/graviti-logo.png';
 const Header = () => {
   return (
     <div className='flex items-center bg-[#ffffff] py-2 sm:px-14 px-4'>
-      <>
+      <a href='#'>
         <img src={ logo } alt='Graviti-logo' className='md:h-14 sm:h-12 h-10' />
-      </>
+      </a>
     </div>
   );
 };
