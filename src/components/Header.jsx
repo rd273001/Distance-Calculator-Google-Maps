eddcac8f1023a2b0bb18772ea6d7d11dafa70be3
rd273001 <rd273001@gmail.com>
2024-07-11 02:20:51 +0530

    "updated logo"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from '../assets/graviti-logo.png';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <div className='flex items-center bg-[#ffffff] py-2 sm:px-14 px-4'>
-      <a href='#'>
-        <img src={ logo } alt='Graviti-logo' className='md:h-14 sm:h-12 h-10' />
+      <a href='#' className='flex items-center'>
+        <img src={ logo } alt='app-logo' className='h-6 mr-1' />
+        <p className='font-bold text-2xl'>Distance Calculator</p>
       </a>
     </div>
   );
